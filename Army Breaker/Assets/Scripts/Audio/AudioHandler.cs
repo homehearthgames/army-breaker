5f6e790b899c5b05f69827b72adccc06aa79f9ff
homehearthgames <homehearthgames@gmail.com>
2023-02-22 22:00:34 -0600

    removed logs in audio manager

--- Army Breaker/Assets/Scripts/Audio/AudioHandler.cs
+++ Army Breaker/Assets/Scripts/Audio/AudioHandler.cs
@@ -26,19 +26,14 @@ public class AudioHandler : MonoBehaviour
     // Called when we switch back to the title scene so the sliders are the same as the current settings
     public void InitializeSliders()
     {
-        Debug.Log("Changig sounds");
         // Update the volume variable in the AudioController based on the selected sound category
         switch (soundCategory)
         {
             case SoundCategory.InterfaceSounds:
                 slider.value = GameManager.Instance.audioController.interfaceSoundsVolume;
-                
-                Debug.Log("Changing music sounds");
                 break;
             case SoundCategory.Music:
                 slider.value = GameManager.Instance.audioController.musicVolume;
-                
-                Debug.Log("Changing interface sounds");
                 Debug.Log(slider.value);
                 break;
             case SoundCategory.GameSounds:
@@ -50,20 +45,14 @@ public class AudioHandler : MonoBehaviour
     // Called when the slider value is changed
     public void OnSliderValueChanged()
     {
-        Debug.Log("Changig sounds");
         // Update the volume variable in the AudioController based on the selected sound category
         switch (soundCategory)
         {
             case SoundCategory.InterfaceSounds:
                 GameManager.Instance.audioController.interfaceSoundsVolume = slider.value;
-                
-                Debug.Log("Changing music sounds");
                 break;
             case SoundCategory.Music:
                 GameManager.Instance.audioController.musicVolume = slider.value;
-                
-                Debug.Log("Changing interface sounds");
-                Debug.Log(slider.value);
                 break;
             case SoundCategory.GameSounds:
                 GameManager.Instance.audioController.gameSoundsVolume = slider.value;
